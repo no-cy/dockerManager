8e272f7182ee45049ac08c29ff6265beee9dfd29
no-cy <nochanyoung0@gmail.com>
2025-07-11 17:49:03 +0900

    스웨거 적용 - spring boot 3.5.3 버전에서는 스웨거가 실행되지 않는 현상이 있어 버전을 3.3.4 버전으로 낮추었음

---
+++ src/main/java/com/example/dockermanager/configuration/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.example.dockermanager.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openApi() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Swagger API")
+                .version("v1.0.0")
+                .description("스웨거 API");
+    }
+}
